5bc85eca7a55286492fc156431bb2c57855cb851
MD.Asif Ullah <mdasifullah456@gmail.com>
2023-09-03 00:10:21 -0700

    update header

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,9 +4,9 @@ import image from "../../assets/images.jpg";
 export const Header = () => {
 
   return (
-    <div className=" mt-10 ml-[10%] mr-[5%]">
+    <div >
       <div className=" flex justify-between items-center w-full h-14 text-3xl font-bold">
-        <h1>Knowledge Cafe</h1>
+        <h1>Programming</h1>
         <img className=" h-14 w-14 rounded-full" src={image} alt="" />
       </div>
     </div>
