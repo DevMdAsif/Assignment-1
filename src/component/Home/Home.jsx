91609f7608299a0f60262a45fbceb0c961c9d8b3
MD.Asif Ullah <mdasifullah456@gmail.com>
2023-09-03 02:57:34 -0700

    updated home.jsx

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,20 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { Knowledge } from "../Knowledge/Knowledge";
 
+import { Knowledge } from "../Knowledge/Knowledge";
+import "./Home.css";
+import { Card } from "../../Card/Card";
 
 export default function Home() {
-  const [knowledge ,setknowledge] = useState([])
+  const [knowledge, setknowledge] = useState([]);
   useEffect(() => {
     fetch("knowledge.json")
       .then((response) => response.json())
       .then((data) => setknowledge(data));
   }, []);
   return (
-    <div className=" pt-8">
-      {
-        knowledge.map((info)=><Knowledge info={info} key={info.id} />)
-      }
-
+    <div className=" pt-8  grid grid-coloum ">
+      <div>
+        {knowledge.map((info) => (
+          <Knowledge info={info} key={info.id} />
+        ))}
+      </div>
+      <div>
+        <Card />
+      </div>
+      
     </div>
-  )
+  );
 }
